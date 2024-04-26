c9b3a515829d8b573ec95c811a375447ea4a9940
Gabriel-MarcoSilva <gabrielmarkolinosilva14@gmail.com>
2024-04-25 23:30:24 -0300

    feat: add nova page, com as funções funcionais

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-const form = document.getElementById('formLogin')
 
 // form.addEventListener('submit', )
 
 function login(e) {
+    const form = document.getElementById('formLogin')
     // se login então document.getelement = display none
 
     e.preventDefault()
@@ -11,7 +11,10 @@ function login(e) {
     const payload = new URLSearchParams(data)
 
     console.log([...payload])
-    
+
+    document.getElementById('dashboard').style.display = 'flex'
+    document.getElementById('login').style.display = 'none'
+
     // fetch("", {
     //     method: "POST",
     //     headers: {
@@ -20,4 +23,51 @@ function login(e) {
     //     body: data
     // }) 
     // .then((res) => res.JSON()).then((data) => form.style.display = 'none')
+}
+
+
+function registroData(e, props) {
+
+    e.preventDefault()
+    let data = ''
+    let payload = null
+    
+    const form = document.getElementById('formData')
+    
+    if (props === 'auto') {
+        data = new Date()
+        
+        payload = new URLSearchParams({
+            dia: data.getDate() + '-' + 0 + Number(data.getMonth() + 1) + '-' + data.getFullYear(),
+            hora: data.getHours() + ':' + data.getMinutes()
+        })
+    } else {
+        data = new FormData(form)
+        payload = new URLSearchParams(data)
+    }
+
+    
+    let info = false;
+    
+    [...payload.values()].map((item) => {
+        if (item == ''){
+            info = true
+        }
+    })
+    
+    if (info){
+        alert('Dados incompletos')
+    } else {
+        document.getElementById('regAuto').style.display = 'flex'
+        document.getElementById('formDasDatas').style.display = 'none'
+    }
+    
+    //enviar payload
+    console.log([...payload.values()])
+    
+}
+
+function revelarForm (){
+    document.getElementById('formDasDatas').style.display = 'flex'
+    document.getElementById('regAuto').style.display = 'none'
 }
